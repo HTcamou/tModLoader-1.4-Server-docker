cba2ad7dbab0aef39676174ce91f170f73e53166
HTcamou <m809311563@gmail.com>
2022-07-01 16:04:37 +0800

    Add files via upload
    添加指定版本号和自动更新环境变量，支持自定义安装版本（当前默认版本为v2022.05.103.34，主要是避免现在自动更新的版本比steam商店版本高的情况），支持自动更新版本开关（true为开启，默认为false），修改默认开服配置文件

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,19 @@
 FROM debian:stretch-slim
 
-#Install dependencies
+ENV Autoupdate=false
+ENV TMOD_VERSION=v2022.05.103.34
+
+EXPOSE 7777
+
 RUN apt update
 
-WORKDIR /home
+WORKDIR /root
 
 #Copy script
 COPY tModLoaderServerStart.sh .
+COPY serverconfig.txt .
 
 #Allow execution
-RUN chmod +x /home/tModLoaderServerStart.sh
+RUN chmod +x /root/tModLoaderServerStart.sh
 
-ENTRYPOINT ["/home/tModLoaderServerStart.sh"]
+ENTRYPOINT ["/root/tModLoaderServerStart.sh"]
--- tModLoaderServerStart.sh
+++ tModLoaderServerStart.sh
@@ -1,38 +1,71 @@
 #!/bin/bash
-apt update
 apt -y install wget unzip libicu-dev
 versiontxt=/home/tModLoader/version.txt
-#最终路径注意更换
-#获取最新版本号
-newversion=$(wget -qO- version.txt -t1 -T2 "https://api.github.com/repos/tModLoader/tModLoader/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
-#输出最新版本号
-echo "latest version: $newversion"
 #获取本地版本号
 localversion=$(cat $versiontxt)
-#输出本地版本号
-echo "local version: $localversion"
-#版本号对比，一致开始启动服务器，不一致开始下载最新版本文件，解压后删除并记录版本号
-if [ "$localversion" == "$newversion" ]
+if [ "$Autoupdate" == "true" ]
     then
-        echo "local version already is latest version"
-    else
-    	#进入下载目录
-        cd /home
-        echo "starting download latest tModLoader version..."
-        #下载最新版压缩包
-        wget https://github.com/tModLoader/tModLoader/releases/download/$newversion/tModLoader.zip
-        echo "Completed download"
-        if [ -f /home/tModLoade/serverconfig.txt ]
-            then 
-                #覆盖解压，不解压serverconfig配置文件（保留配置文件）
-                unzip -o tModLoader.zip -d tModLoader/ -x "serverconfig.txt"
+        #最终路径注意更换
+        #获取最新版本号
+        newversion=$(wget -qO- version.txt -t1 -T2 "https://api.github.com/repos/tModLoader/tModLoader/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+        #输出最新版本号
+        echo "latest version: $newversion"
+        #输出本地版本号
+        echo "local version: $localversion"
+        #版本号对比，一致开始启动服务器，不一致开始下载最新版本文件，解压后删除并记录版本号
+        if [ "$localversion" == "$newversion" ]
+            then
+                echo "local version already is latest version"
             else
-                unzip -o tModLoader.zip -d tModLoader/
+    	        #进入下载目录
+                cd /home
+                echo "starting download latest tModLoader version..."
+                #下载最新版压缩包
+                wget https://github.com/tModLoader/tModLoader/releases/download/$newversion/tModLoader.zip
+                echo "Completed download"
+                if [ -f /home/tModLoade/serverconfig.txt ]
+                    then 
+                        #覆盖解压，不解压serverconfig配置文件（保留配置文件）
+                        unzip -o tModLoader.zip -d tModLoader/ -x "serverconfig.txt"
+                    else
+                        unzip -o tModLoader.zip -d tModLoader/
+                fi
         fi
         #删除下载压缩包
         rm -f tModLoader.zip
         #注释当前版本
         echo "$newversion" > "$versiontxt"
+    else
+        if [ "$localversion" == "$TMOD_VERSION" ]
+            then
+                echo "local version already is ENV TMOD_VERSION:$TMOD_VERSION"
+            else
+    	        #进入下载目录
+                cd /home
+                echo "starting download ENV TMOD_VERSION:$TMOD_VERSION tModLoader version..."
+                #下载最新版压缩包
+                wget https://github.com/tModLoader/tModLoader/releases/download/$TMOD_VERSION/tModLoader.zip
+                echo "Completed download"
+                if [ -f /home/tModLoade/serverconfig.txt ]
+                    then 
+                        #覆盖解压，不解压serverconfig配置文件（保留配置文件）
+                        unzip -o tModLoader.zip -d tModLoader/ -x "serverconfig.txt"
+                    else
+                        unzip -o tModLoader.zip -d tModLoader/
+                fi
+        fi
+        #删除下载压缩包
+        rm -f tModLoader.zip
+        #注释当前版本
+        echo "$TMOD_VERSION" > "$versiontxt"
+fi
+#创建MOD WORLD目录
+cd /home/tModLoader
+mkdir Mods Mods/ModPacks Worlds
+cd /root
+if [ -f "serverconfig.txt" ]
+    then
+    mv serverconfig.txt /home/tModLoader
 fi
 #执行开服脚本
 cd /home/tModLoader
